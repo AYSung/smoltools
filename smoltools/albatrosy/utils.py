a7ddca3ae2904b1ed684efabaee1a4aa4f7d81fb
Andrew Sung <aysung300@gmail.com>
2022-06-19 09:17:16 -0500

    docs: docstrings for albatrosy utils

--- smoltools/albatrosy/utils.py
+++ smoltools/albatrosy/utils.py
@@ -6,6 +6,21 @@ def extract_residue_number(s: pd.Series) -> pd.Series:
 
 
 def splice_conformation_tables(df_a: pd.DataFrame, df_b: pd.DataFrame) -> pd.DataFrame:
+    """Splice distance tables for two conformations together.
+
+    Parameters:
+    -----------
+    df_a (DataFrame): Dataframe with the atom IDs (residue number, carbon ID) of each
+        atom pair and the distance (in angstroms) between each pair.
+    df_b (DataFrame): Dataframe with the atom IDs (residue number, carbon ID) of each
+        atom pair and the distance (in angstroms) between each pair.
+
+    Returns:
+    --------
+    DataFrame: DataFrame with the lower triangle of the DataFrame containing values
+        from the first conformation and the upper triangle of the DataFrame containing
+        values from the second conformation.
+    """
     return pd.concat(
         [
             df_a.loc[
