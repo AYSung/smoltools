109e1eb7bee59761f540804be7ffd23df878ffe1
Andrew Sung <aysung300@gmail.com>
2022-07-09 16:55:11 -0500

    feat: add in error handling for empty residue/atom lists

--- smoltools/pdbtools/exceptions.py
+++ smoltools/pdbtools/exceptions.py
@@ -1,6 +1,3 @@
-from Bio.PDB.Chain import Chain
-
-
 class ChainNotFound(KeyError):
     def __init__(self, structure_id: str, model_id: str, chain_id: str):
         message = f'Chain {structure_id}/{model_id}/{chain_id} not in structure'
@@ -8,9 +5,12 @@ class ChainNotFound(KeyError):
 
 
 class NoResiduesFound(ValueError):
-    def __init__(self, chain: Chain) -> None:
-        chain_id = chain.get_id()
-        model_id = chain.get_parent().get_id()
-        structure_id = chain.get_parent().get_parent().get_id()
-        message = f'No residues matching filter criteria found in {structure_id}/{model_id}/{chain_id}'
+    def __init__(self) -> None:
+        message = 'No residues matching filter criteria found.'
+        super().__init__(message)
+
+
+class NoAtomsFound(ValueError):
+    def __init__(self) -> None:
+        message = 'No atoms matching filter criteria found.'
         super().__init__(message)
