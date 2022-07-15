99ac215add8aedff8fc66813b870f51305677a71
Andrew Sung <aysung300@gmail.com>
2022-07-15 13:01:36 -0500

    refactor: remove default labeled atom dict

--- smoltools/albatrosy/main.py
+++ smoltools/albatrosy/main.py
@@ -83,7 +83,7 @@ def coordinates_from_chain(
 
 def coordinates_from_path(
     path: str,
-    labeled_atoms: dict[str, list[str]] = LABELED_CARBONS['ILV'],
+    labeled_atoms: dict[str, list[str]],
     model: int = 0,
     chain: str = 'A',
 ) -> pd.DataFrame:
